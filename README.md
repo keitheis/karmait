0b5ae968a3b28ea606f96f52ab51a10e0c19373e
Keith Yang <yang@keitheis.org>
2015-12-09 21:33:29 +0800

    Add some README; add meetup knot.

---
+++ README.md
@@ -0,0 +1,6 @@
+[業障](https://www.moedict.tw/%E6%A5%AD%E9%9A%9C)它 
+
+It helps:
+
+ - Know yourself better.
+ - Remove unwanted karma.
---
+++ karmait/knots/meetup.py
@@ -0,0 +1,8 @@
+
+
+class Meetup:
+    def get_rsvp(since=None, until=None):
+        pass
+
+    def count_karma(since=None, until=None):
+        pass
